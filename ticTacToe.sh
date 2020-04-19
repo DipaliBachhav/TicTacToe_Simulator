15e70c69dd70ce245f58430b8a74f1af1cadd48c
DipaliBachhav <dipa2718@gmail.com>
2020-04-19 21:56:07 +0530

    Added Winning Conditions For Player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -40,7 +40,84 @@ function printBoard()
 	echo " --------------"
 }
 
+function checkHorizontalCase(){
+	if [ $winner == false ]
+	then
+		numberOfRows=3
+		position=1
+		counter=1
+		while [ $counter -le $numberOfRows ]
+		do
+			if [[ ${board[$position]} == ${board[$position+1]} ]] && [[ ${board[$position+1]} == ${board[$position+2]} ]] && [[ ${board[$position+2]} == ${board[$position]} ]] && [[ ${board[$position]} == $1 ]]
+			then
+				printBoard
+				echo "$1 wins Horizontal"
+				winner=true
+				break
+			else
+				position=$(( $position+$numberOfRows ))
+			fi
+		counter=$(( $counter+1 ))
+		done
+	fi
+}
+function checkVerticalCase(){
+	if [ $winner == false ]
+	then
+		numberOfColumns=3
+		position=1
+		counter=1
+		while [ $counter -le $numberOfColumns ]
+		do
+			if [[ ${board[$position]} == ${board[$position+3]} ]] && [[ ${board[$position+3]} == ${board[$position+6]} ]] && [[ ${board[$position+6]} == ${board[$position]} ]] && [[ ${board[$position]} == $1 ]]
+			then
+				printBoard
+				echo "$1 wins Vertical"
+				winner=true
+				break
+			else
+				position=$(( $position+1))
+			fi
+		counter=$(( $counter+1 ))
+		done
+	fi
+}
+function checkDiagonalCase(){
+	if [ $winner == false ]
+	then
+		position=1
+		if [[ ${board[$position]} == ${board[$position+4]} ]] && [[ ${board[$position+4]} == ${board[$position+8]} ]] && [[ ${board[$position+8]} == ${board[$position]} ]] && [[ ${board[$position]} == $1 ]]
+			then
+				printBoard
+				echo "$1 wins Diagonal"
+				winner=true
+		elif [[ ${board[$position+2]} == ${board[$position+4]} ]] && [[ ${board[$position+4]} == ${board[$position+6]} ]] && [[ ${board[$position+6]} == ${board[$position+2]} ]] && [[ ${board[$position+2]} == $1 ]] 
+			then
+				printBoard
+				echo "$1 wins Diagonal"
+				winner=true
+		fi
+	fi
+}
+
+function userInput(){
+	row=1
+	column=3
+	echo "user is playing"
+	read -p "Enter position in between 1-9: " position
+	if [[ ${board[$position]} == $tail ]]
+	then
+		echo "player turn"
+		board[$position]=$player
+		turn=$(( $turn+1 ))
+	else
+		echo "Invalid input"
+		userInput
+	fi
+	playerTurn=false
+}
+
 resetBoard
 assignedLetter
 checkWhoPlayFirst
-printBoard
+
